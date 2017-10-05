9ad09849271db9f11745755452fdf8bada6dd722
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-10-05 09:41:00 +0200

    1º modificación

--- src/cod_holamundo/COD_holamundo.java
+++ src/cod_holamundo/COD_holamundo.java
@@ -3,7 +3,11 @@ package cod_holamundo;
 public class COD_holamundo {
     
     public static void main(String[] args) {
-        System.out.println("Hola mi nombre es programa");
+        
+        
+        System.out.println("Hola");
+                
+        System.out.println("adios");        
     }
     
 }
