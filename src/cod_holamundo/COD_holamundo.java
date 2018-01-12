15f1a2b46202f6278a158fea5a721c57b1409c6b
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-12 09:39:08 +0100

    Rama creada

--- src/cod_holamundo/COD_holamundo.java
+++ src/cod_holamundo/COD_holamundo.java
@@ -2,12 +2,13 @@ package cod_holamundo;
 
 public class COD_holamundo {
     
-    public static void main(String[] args) {
-        
-        
+    public static void main(String[] args) {        
+       
         System.out.println("Hola");
                 
-        System.out.println("adios");        
+        System.out.println("adios");   
+        
+        
     }
     
 }
